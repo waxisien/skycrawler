5d647ca812e12d86973911e07e3a26f0788e54ca
Florian Nitzsche <flo.nitzsche@gmail.com>
2017-08-12 15:16:13 -0400

    Build static vendor files

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulpBrowser = require("gulp-browser");
 var reactify = require('reactify');
 var del = require('del');
 var size = require('gulp-size');
+var mainBowerFiles = require('gulp-main-bower-files');
+var inject = require('gulp-inject');
 
 // tasks
 gulp.task('transform', function () {
@@ -15,10 +17,28 @@ gulp.task('transform', function () {
 });
 
 gulp.task('del', function () {
-    return del(['./skycrawler/static/scripts/js']);
+    return del(['./skycrawler/static/scripts/js', './skycrawler/static/scripts/vendor']);
 });
 
 gulp.task('default', ['del'], function() {
   gulp.start('transform');
-  gulp.watch('./skycrawler/static/scripts/jsx/*.js', ['transform']);
+  gulp.start('build');
+  //gulp.watch('./skycrawler/static/scripts/jsx/*.js', ['transform']);
+});
+
+gulp.task('build', function () {
+  // copy vendor files from /bower_compontents to /assets/vendors
+  var bowerStreamJS = gulp.src('./bower.json')
+        .pipe(mainBowerFiles('**/*.js'))
+        .pipe(gulp.dest('./skycrawler/static/scripts/vendor'));
+
+  // send bower bower scripts
+  gulp.src('./skycrawler/templates/map.html')
+  .pipe(
+    inject(bowerStreamJS, {relative: true, name: 'bower'})
+  )
+  // save the file
+  .pipe(
+    gulp.dest('./skycrawler/templates')
+  );
 });
