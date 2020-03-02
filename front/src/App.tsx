2897ecda76590122426fdd2c494597a2758a7773
Florian Nitzsche <flo.nitzsche@gmail.com>
2020-03-02 17:01:35 -0500

    Button to switch view

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,32 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { IconButton } from '@material-ui/core';
+import PlaceOutlinedIcon from '@material-ui/icons/PlaceOutlined';
+import ViewListOutlinedIcon from '@material-ui/icons/ViewListOutlined';
 
 import { client } from './lib/graphql';
 import Buildings from './Buildings';
 
-const App = (): JSX.Element => (
-  <ApolloProvider client={client}>
-    <Buildings/>
-  </ApolloProvider>
-)
+import './App.css';
+
+const App = (): JSX.Element => {
+
+  const [isViewMap, setIsViewMap] = useState(false);
+
+  const toggleMenu = (): void => setIsViewMap(!isViewMap);
+
+  return (
+    <ApolloProvider client={client}>
+      <header className="header">
+        <IconButton aria-label="menu" color="default" onClick={toggleMenu}>
+          {!isViewMap && <PlaceOutlinedIcon style={{ fill: 'white' }}/>}
+          {isViewMap && <ViewListOutlinedIcon style={{ fill: 'white' }}/>}
+        </IconButton>
+      </header>
+      {!isViewMap && <Buildings/>}
+      {isViewMap && 'Incoming...'}
+    </ApolloProvider>
+  );
+};
 
 export default App;
