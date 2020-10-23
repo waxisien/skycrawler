3f8c7730e59f6af69cde02c2f5f7f044ebadb7df
waxisien <flo.nitzsche@gmail.com>
2020-10-23 10:41:51 -0400

    Update list from map boundaries

--- front/src/MapViewLayout.tsx
+++ front/src/MapViewLayout.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Bounds } from 'google-map-react';
 
 import MapView from "./MapView"
 import MapViewList from './MapViewList';
 
-const MapViewLayout = (): JSX.Element => { 
+const MapViewLayout = (): JSX.Element => {
+  // TODO: use store
+  const [bounds, setBounds] = React.useState<Bounds>();
+
+  const onBoundsChange = (_bounds: Bounds) => setBounds(_bounds);
+
   return (
     <div className="map-view-layout">
       <div className="map-view-column">
-        <MapView/>
+        <MapView onBoundsChange={onBoundsChange}/>
       </div>
       <div className='map-view-list-column'>
-        <MapViewList />
+        <MapViewList mapBounds={bounds}/>
       </div>
     </div>
   );
