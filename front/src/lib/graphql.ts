af40b8c0e8fe6708c9eed996839b174903f0ac19
waxisien <flo.nitzsche@gmail.com>
2020-10-23 13:38:28 -0400

    Upgrade apollo and graphql

--- front/src/lib/graphql.ts
+++ front/src/lib/graphql.ts
@@ -1,5 +1,6 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
 });
