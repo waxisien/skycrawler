fa0b700f80cd198d087db11726ba04b2462b1323
waxisien <flo.nitzsche@gmail.com>
2020-10-30 11:25:51 -0400

    Change menu

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { IconButton } from '@material-ui/core';
 import PlaceOutlinedIcon from '@material-ui/icons/PlaceOutlined';
-import ViewListOutlinedIcon from '@material-ui/icons/ViewListOutlined';
 import InfoOutlined from '@material-ui/icons/InfoOutlined';
+import Button from '@material-ui/core/Button';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 import { client } from './lib/graphql';
 import BuildingListView from './BuildingListView';
@@ -14,27 +20,35 @@ import './App.css';
 
 const App = (): JSX.Element => {
 
-  const [isViewMap, setIsViewMap] = React.useState(true);
   const [infoDialogOpen, setInfoDialogOpen] = React.useState(false);
 
-  const toggleMenu = (): void => setIsViewMap(!isViewMap);
   const toggleDialogInfo = (): void => setInfoDialogOpen(!infoDialogOpen);
 
   return (
     <ApolloProvider client={client}>
-      <header className="header">
-        <IconButton aria-label="menu" color="default" onClick={toggleMenu}>
-          {!isViewMap && <PlaceOutlinedIcon style={{ fill: 'white' }}/>}
-          {isViewMap && <ViewListOutlinedIcon style={{ fill: 'white' }} />}
-        </IconButton>
-        Skycrawler
-        <IconButton className='info-dialog-icon' aria-label="menu" color="default" onClick={toggleDialogInfo}>
-          <InfoOutlined style={{ fill: 'white' }} />
-        </IconButton>
-        <InfoDialog open={infoDialogOpen} onClose={toggleDialogInfo}/>
-      </header>
-      {!isViewMap && <BuildingListView/>}
-      {isViewMap && <MapViewLayout/>}
+      <Router>
+        <header className="header">
+          <IconButton aria-label="menu" color="default" href="/">
+            <PlaceOutlinedIcon style={{ fill: 'white' }}/>
+          </IconButton>
+          <Link to="/">Skycrawler</Link>
+          <Button variant="outlined" href="/list" className="menu-list-button">
+            View list
+          </Button>
+          <IconButton className='info-dialog-icon' aria-label="menu" color="default" onClick={toggleDialogInfo}>
+            <InfoOutlined style={{ fill: 'white' }} />
+          </IconButton>
+          <InfoDialog open={infoDialogOpen} onClose={toggleDialogInfo}/>
+        </header>
+        <Switch>
+          <Route path="/list">
+            <BuildingListView />
+          </Route>
+          <Route path="/">
+            <MapViewLayout />
+          </Route>
+        </Switch>
+      </Router>
     </ApolloProvider>
   );
 };
