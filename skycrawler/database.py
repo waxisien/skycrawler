2a8cad1d1092a01ed376cbb0dc0518b51d8c4aad
waxisien <flo.nitzsche@gmail.com>
2020-10-30 10:49:06 -0400

    Do not check same thread for sqlite connection

--- skycrawler/database.py
+++ skycrawler/database.py
@@ -4,7 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///'+os.environ['SKYCRAWLER_DB'], convert_unicode=True)
+# 'check_same_thread=false': as long as only one thread writes in db (searchengine script) we are safe.
+sqlite_url = 'sqlite:///' + os.environ['SKYCRAWLER_DB'] + '?check_same_thread=false'
+engine = create_engine(sqlite_url, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
