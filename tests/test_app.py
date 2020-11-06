1e00c80fe1480be744ab4bbcdfe9f3f8ef2442f9
waxisien <flo.nitzsche@gmail.com>
2020-11-06 08:49:45 -0500

    Remove legacy code

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,6 @@
-import os
-import pytest
-
-os.environ["MY_GOOGLE_MAP_KEY"] = 'test'
-
-from skycrawler.app import app
-
-
-@pytest.fixture
-def app_client():
-    return app.test_client()
-
-
-def test_index(app_client, data_test):
-    page = app_client.get('/')
-    assert page.status_code == 200
-
-
-def test_raw(app_client, data_test):
-    page = app_client.get('/raw')
+def test_graphql_route(app_client):
+    page = app_client.post(
+        '/graphql',
+        data={'query': '{buildings{name}}'},
+    )
     assert page.status_code == 200
