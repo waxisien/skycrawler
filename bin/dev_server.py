3500f2c12c1fc35ca74c82bad679a1679918f0da
waxisien <flo.nitzsche@gmail.com>
2020-11-06 08:49:45 -0500

    Remove legacy code

--- bin/dev_server.py
+++ bin/dev_server.py
@@ -1,4 +1,5 @@
 from skycrawler.app import create_app
 
-app = create_app()
-app.run(debug=True)
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
--- skycrawler/app.py
+++ skycrawler/app.py
@@ -1,10 +1,7 @@
-import os
-
-from flask import Flask, request, Response, jsonify, render_template, Blueprint
+from flask import Blueprint,  Flask
 from flask_cors import CORS
 from flask_graphql import GraphQLView
 
-from skycrawler.model import Building
 from skycrawler.schema import schema
 
 
@@ -19,30 +16,3 @@ def create_app():
     app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
 
     return app
-
-
-@main.route('/raw', methods=['GET'])
-def raw():
-
-    data = Building.query.order_by(Building.height.desc()).filter(Building.is_active == 1).limit(15).all()
-
-    return render_template('raw.html', buildings=data)
-
-
-@main.route('/', methods=['GET'])
-def index():
-
-    buildings = Building.query.order_by(Building.height.desc()).filter(Building.is_active == 1).limit(1000).all()
-
-    data = []
-    for building in buildings:
-        data.append({'name': building.name,
-                     'city': building.city.name,
-                     'height': building.height,
-                     'latitude': building.city.latitude,
-                     'longitude': building.city.longitude,
-                     'link': building.link,
-                     'status': building.status,
-                     })
-
-    return render_template('map.html', buildings=data, MY_GOOGLE_MAP_KEY=os.environ['MY_GOOGLE_MAP_KEY'])
